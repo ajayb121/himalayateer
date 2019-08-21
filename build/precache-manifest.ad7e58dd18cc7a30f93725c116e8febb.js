d064e646682d8dc4730448ada272cc632128a2a6
ajayb121git config --global user.email ajaybrahma93@gmail.com <ajaybrahma@Ajays-MacBook-Air.local>
2019-08-22 03:15:20 +0530

    add build

---
+++ build/precache-manifest.ad7e58dd18cc7a30f93725c116e8febb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bcb167f0b2d3a6f9d9e7069748fe826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ecd80e8ea38e7ac6ad0",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2a1bcc4608a7412606c4",
+    "url": "/static/js/2.63497c2b.chunk.js"
+  },
+  {
+    "revision": "5ecd80e8ea38e7ac6ad0",
+    "url": "/static/js/main.31128233.chunk.js"
+  },
+  {
+    "revision": "db6be01aac7cb1c6d4ff",
+    "url": "/static/js/runtime~main.0033641b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -19,10 +19,10 @@ mongoose
 // Use Routes
 app.use('/api/items', items);
 
-app.use(express.static('client/build'));
+app.use(express.static('build'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 80;
