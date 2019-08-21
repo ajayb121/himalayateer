0623b88e6dae70b3a289e9b272587f07e544d1aa
ajayb121git config --global user.email ajaybrahma93@gmail.com <ajaybrahma@Ajays-MacBook-Air.local>
2019-08-22 02:55:09 +0530

    new changes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb+srv://ajayb121:ajayb121@cluster0-thj7y.gcp.mongodb.net/test?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.json());
+
+const items = require('./routes/api/items');
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('Mongo DB connected...'))
+  .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
+
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 80;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
